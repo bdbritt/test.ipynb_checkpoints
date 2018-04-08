373e3846844a4c99dea642e0fc5b9dc79cb628c0
bdbritt <37956984+bdbritt@users.noreply.github.com>
2018-04-08 07:31:47 -0400

    added some changes. also testing

--- cis4150_proj.py
+++ cis4150_proj.py
@@ -3,19 +3,24 @@ import glob
 import os
 import shutil
 
-
-PATH = r'D:\Scripts\school_project\data'
-allFiles = glob.glob(PATH + "/*.csv")
-
-#print(os.path.exists('D:\Scripts\school_project\data'))
+import openpyxl
 
 
-merged_data = 'merged_out.csv'
+# PATH = r'D:\Scripts\school_project\data'
+# allFiles = glob.glob(PATH + "/*.csv")
+#
+# #print(os.path.exists('D:\Scripts\school_project\data'))
+#
+#
+# merged_data = 'merged_out.csv'
+#
+# with open(merged_data, 'wb') as outfile:
+#     for i, fname in enumerate(allFiles):
+#         with open(fname, 'rb') as infile:
+#             if i != 0:
+#                 infile.readline()
+#             shutil.copyfileobj(infile, outfile)
+#             print(fname + " has been imported.")
 
-with open(merged_data, 'wb') as outfile:
-    for i, fname in enumerate(allFiles):
-        with open(fname, 'rb') as infile:
-            if i != 0:
-                infile.readline()
-            shutil.copyfileobj(infile, outfile)
-            print(fname + " has been imported.")
+wb = openpyxl.load_workbook('D:\Scripts\school_project\data\PPA Opportunity Tableau ACO Detail Testing.xlsx')
+print(wb.get_sheet_names())
